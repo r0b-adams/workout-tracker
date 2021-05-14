57d9117a441167e9e1a47b55dfd3b65da0178ec5
Robert Adams <adamsiii.robert@gmail.com>
2021-05-13 20:33:01 -0700

    moved methods to workoutsRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,35 +1,7 @@
 const router = require('express').Router();
+const workoutsRoutes = require('./workoutsRoutes');
 
-
-
-// get last workout
-// localhost:3001/api/workouts
-router.get('/workouts', async (req, res) => {
-    console.log("get last workout ping");
-    console.log(req.body);
-});
-
-// add exercise (to an existing workout)
-// localhost:3001/api/workouts
-router.put('/workouts', async (req, res) => {
-    console.log("add exercise ping");
-    console.log(req.body);
-
-});
-
-// create workout
-// localhost:3001/api/workouts
-router.post('/workouts', async (req, res) => {
-    console.log("create workout ping");
-    console.log(req.body);
-
-});
-
-// get workouts in range
-// localhost:3001/api/workouts/range
-router.get('/workouts/range', async (req, res) => {
-    console.log("get workouts in range ping");
-    console.log(req.body);
-});
+// localhost:3001/api/workout
+router.use('/workouts', workoutsRoutes);
 
 module.exports = router;
