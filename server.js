3042219841a9de2d47de3b989f69d1f7006b872c
Robert Adams <adamsiii.robert@gmail.com>
2021-05-13 20:32:11 -0700

    added comments, added controllers to app for routing

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const path = require('path');
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const path = require('path');                  // The path module helps interact with the file system (part of Node.js)
+const express = require("express");            // server functions
+const logger = require("morgan");              // see http reqs
+const mongoose = require("mongoose");          // db and schema based models
+const controllers = require("./controllers");  // require controllers for routing
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,20 +12,18 @@ const app = express();
 
 app.use(logger("dev"));
 
+// middleware that helps with data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(controllers); // tells express to use routing
+
+// deliver staic assets from these folders
 app.use(express.static('public'));
 app.use(express.static('views'));
 
+// connect to database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true }); //boilerplate (replace populate with db name)
-
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
-
-
-// let's start by getting the page routes to work
 
 app.get('/exercise', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/exercise.html'));
@@ -34,8 +33,11 @@ app.get('/stats', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/stats.html'));
 });
 
-// If no matching route is found default to home
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
 
